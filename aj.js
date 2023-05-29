02efce5223e96bc2977e73a051163e64500a71a8
Noufal Vattaparambil <noufalvp9809@gmail.com>
2023-05-29 19:53:32 +0400

    second commit

---
+++ aj.js
@@ -0,0 +1,46 @@
+//1
+console.log("welcome js");
+let a=23;
+let b=34;
+let sum=a+b;
+console.log("result is = ",sum);
+//2
+let c=55;
+let d=36;
+let result=c-d;
+console.log("result is = ",result);
+//3
+let e=34;
+let f=25;
+let mark=e*f;
+console.log("result is = ",mark);
+//4
+let m=15;
+let n=5;
+let tota=m/n;
+console.log("result is = ",tota);
+//5
+let g=17;
+let h=3;
+let mar=g%h;
+console.log("result is = ",mar);
+//6
+let i=45;
+let j=34;
+let k=23;
+let l=3;
+let resu=i+j*k-l;
+console.log("result is = ",resu);
+//7
+let sub1=70;
+let sub2=80;
+let sub3=90;
+let sub4=50;
+let total=sub1+sub2+sub3+sub4;
+console.log("total mark = ",total);
+if(total>=250){
+    console.log("passed")
+}
+else{
+    console.log("failed")
+}
